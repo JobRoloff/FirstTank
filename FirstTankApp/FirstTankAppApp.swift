7f8ad3bd155e5ce30d72959ec1c0b2feab711733
JobRoloff <61367131+JobRoloff@users.noreply.github.com>
2023-03-01 10:51:28 -0600

    Add files via upload

---
+++ FirstTankApp/FirstTankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstTankAppApp.swift
+//  FirstTankApp
+//
+//  Created by job on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstTankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
